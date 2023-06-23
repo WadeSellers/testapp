1fd85aefd87ba2c756684619de9ccc844601d23c
Wade Sellers <wade@wadesellers.com>
2023-06-23 12:34:13 -0400

    Initial Commit

---
+++ testapp/testappApp.swift
@@ -0,0 +1,17 @@
+//
+//  testappApp.swift
+//  testapp
+//
+//  Created by Wade Sellers on 6/23/23.
+//
+
+import SwiftUI
+
+@main
+struct testappApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
